83d4bbf10447d554efc9c22dac8c4b1371757aae
金晖 <55843852+pphui8@users.noreply.github.com>
2021-10-09 23:25:18 +0800

    Add files via upload

---
+++ animeLoop.js
@@ -0,0 +1,23 @@
+window.onload = animeLoop();
+
+function animeLoop() {
+    sleep(1000);
+    setInterval()
+}
+
+function Loop() {
+    // set text value
+
+    // wait for while
+    sleep(1000);
+    // start
+
+    // flip
+
+    // show
+
+    // flip back
+
+    //wait for a while
+    sleep(1000);
+}
